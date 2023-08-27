53c4d3666ed2c96959899b4c69e5f7755c4d94a5
Vinicios  Weide Ebling <127418830+ViniciosWE@users.noreply.github.com>
2023-08-27 13:39:51 -0300

    Add files via upload

---
+++ conteudos/c/Aula17_08/Exemplo1.java
@@ -0,0 +1,12 @@
+
+package c.Aula17_08;
+
+import java.util.Scanner;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+  
+}
